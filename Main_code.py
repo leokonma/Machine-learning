1b9fe09ba8bd75004136e0c497aee7cd9b8c53f1
leokonma <leodomo1050@gmail.com>
2025-09-17 14:34:31 +0000

    final tables and insertions

--- Main_code.py
+++ Main_code.py
@@ -10,6 +10,7 @@ tables = {
     "player_performances": pd.read_csv("/workspaces/Machine-learning/data/raw/player_performances/player_performances.csv", sep=","),
     "team_competitions_seasons": pd.read_csv("/workspaces/Machine-learning/data/raw/team_competitions_seasons/team_competitions_seasons.csv", sep=","),
     "team_details": pd.read_csv("/workspaces/Machine-learning/data/raw/team_details/team_details.csv", sep=","),
+    "player_profiles" : pd.read_csv("/workspaces/Machine-learning/data/raw/player_profiles/player_profiles.csv", sep=",")
 }
 for name, df in tables.items():
     print(f"\n=== {name.upper()} ===")
@@ -25,13 +26,10 @@ for name, df in tables.items():
     print("Column types:\n", df.dtypes)    
     print("Summary stats:\n", df.describe(include='all').transpose().head(10))
 
-injured = tables["player_injuries"]   
-
+player_injured = tables["player_injuries"]   
 market_value = tables["player_latest_market_value"]
-
-player_ind_perf = tables["player_performances"]
-
-team_info = tables["team_competitions_seasons"]
-
+player_performance = tables["player_performances"]
+team_season = tables["team_competitions_seasons"]
 team_details = tables["team_details"]
+player_profiles = tables["player_profiles"]
 
