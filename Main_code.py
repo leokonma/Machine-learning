a961d1890a714a485de85fdac3d07b8b46c3afaa
leokonma <leodomo1050@gmail.com>
2025-09-17 14:31:47 +0000

    Update main code, remove notebook, update download script

--- Main_code.py
+++ Main_code.py
@@ -7,14 +7,9 @@ import seaborn as sns
 tables = {
     "player_injuries": pd.read_csv("/workspaces/Machine-learning/data/raw/player_injuries/player_injuries.csv", sep=","),
     "player_latest_market_value": pd.read_csv("/workspaces/Machine-learning/data/raw/player_latest_market_value/player_latest_market_value.csv", sep=","),
-    "player_market_value": pd.read_csv("/workspaces/Machine-learning/data/raw/player_market_value/player_market_value.csv", sep=","),
-    "player_national_performances": pd.read_csv("/workspaces/Machine-learning/data/raw/player_national_performances/player_national_performances.csv", sep=","),
     "player_performances": pd.read_csv("/workspaces/Machine-learning/data/raw/player_performances/player_performances.csv", sep=","),
-    "player_teammates_played_with": pd.read_csv("/workspaces/Machine-learning/data/raw/player_teammates_played_with/player_teammates_played_with.csv", sep=","),
-    "team_children": pd.read_csv("/workspaces/Machine-learning/data/raw/team_children/team_children.csv", sep=","),
     "team_competitions_seasons": pd.read_csv("/workspaces/Machine-learning/data/raw/team_competitions_seasons/team_competitions_seasons.csv", sep=","),
     "team_details": pd.read_csv("/workspaces/Machine-learning/data/raw/team_details/team_details.csv", sep=","),
-    "transfer_history": pd.read_csv("/workspaces/Machine-learning/data/raw/transfer_history/transfer_history/transfer_history.csv", sep=","),
 }
 for name, df in tables.items():
     print(f"\n=== {name.upper()} ===")
@@ -39,3 +34,4 @@ player_ind_perf = tables["player_performances"]
 team_info = tables["team_competitions_seasons"]
 
 team_details = tables["team_details"]
+
--- download_data.py
+++ download_data.py
@@ -7,15 +7,10 @@ FOLDERS = {
     # EXAMPLES — replace with your real IDs
     "player_injuries": "1ZDfFfdR2ZOBNuHov2g6nbOWh0QdEXhRX",
     "player_latest_market_value": "16KeVV_KpdOiHjgNrGKROtlXrQuI5AhRg",
-    "player_market_value": "17HJHRDVIqGhARlCqxkLag68QR3thT1Oh",
-    "player_national_performances": "1Pmwbpsen8UWsOYbac7wAjt7TlBUSX-kC",
     "player_performances": "1o_mmxALx-JTPmjGbRTv0oixtff7uQzSR",
     "player_profiles": "1Otah8KtCGIR-5xeSBbMI6xBn3gt8q_La",
-    "player_teammates_played_with": "1FTjADdSdEWi-5EHJImQEsfmn_Wnxa8ys",
-    "team_children": "1OJ5G0AeDTD3G7RnzBA3ybOcUD1PKjOUC",
     "team_competitions_seasons": "1W29kM_rvS2IWAw6FXzlFzDFRLt_zSNjR",
     "team_details": "1NI1A__ZNfd-dFDXeTWJzX3GO0B9H_McZ",
-    "transfer_history": "1UysW0OF00PuCDoHhY-OIp6lXwsOzVV-X",
 }
  
 RAW = Path("data/raw")
